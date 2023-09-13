d40d5555e68acd91ad3701b93cb1a75528c1e4c0
balogun14 <awwalbalogun06@gmail.com>
2023-09-13 15:28:44 +0100

    i made it that users don't need to specify author

--- todoApp/views.py
+++ todoApp/views.py
@@ -6,39 +6,55 @@ from django.views.generic import (
     UpdateView,
     DetailView,
 )
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
 from .models import TodoModel
 
 
-class TodoListView(ListView):
+class TodoListView(LoginRequiredMixin,ListView):
     model = TodoModel
     template_name = "home.html"
 
 
-class TodoCreateView(CreateView):
+class TodoCreateView(LoginRequiredMixin,CreateView):
     model = TodoModel
     context_object_name = "todo"
     template_name = "todo_create.html"
-    fields = ["text", "author"]
+    fields = ["text"]
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class TodoDeleteView(DeleteView):
+class TodoDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     context_object_name = "todo"
     model = TodoModel
     template_name = "todo_delete.html"
     success_url = reverse_lazy("home")
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class TodoUpdateView(UpdateView):
+class TodoUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     context_object_name = "todo"
     model = TodoModel
     template_name = "todo_edit.html"
-    fields = ["text", "author"]
+    fields = ["text"]
+
+    def test_func(self):
+        """
+        docstring
+        """
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class TodoDetailView(DetailView):
+class TodoDetailView(LoginRequiredMixin,DetailView):
     context_object_name = "todo"
     model = TodoModel
     template_name = "todo_detail.html"
